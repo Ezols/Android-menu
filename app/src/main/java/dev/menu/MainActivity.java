37f9fb09b844d75347ce6b11002d2e7bdad0c71a
Ozols <its27eo1@odin.local>
2017-07-25 10:59:45 +0300

    Added OncreateOptionmenu and OnOptionsItemSelected

--- app/src/main/java/dev/menu/MainActivity.java
+++ app/src/main/java/dev/menu/MainActivity.java
@@ -2,8 +2,39 @@ package dev.menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menuHere) {
+        // Link to xml menu
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menuHere);
+
+        return super.onCreateOptionsMenu(menuHere);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Get main_menu items
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId())
+        {
+            case R.id.settings:
+                Log.i("Menu item selected", "Settings");
+                return true;
+            case R.id.help:
+                Log.i("Menu item selected", "Help");
+                return true;
+            default:
+                return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
